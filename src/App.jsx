6bdfea89adbf78af2cdd215710f7e1ca00cffd0d
KingEjikeme <kingsonejikeme@gmail.com>
2024-05-05 01:24:13 +0100

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section>Parallax</section>
+      <section id="Services">Services</section>
+      <section>Parallax</section>
+      <section id="Portfolio">Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+    </div>
+  );
 };
 
 export default App;
